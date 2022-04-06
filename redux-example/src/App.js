ce1973266088212421c3b6a6dff6b7ea5a4efc5c
rubenavaldez <rubenavaldez@yahoo.com>
2022-04-06 12:26:19 -0700

    added redux store

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -1,15 +1,18 @@
-
 import './App.css';
 import Post from './components/Post'
 import PostForm from './components/PostFrom';
 
+import store from "./store"
+
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <PostForm/>
       <hr/>
       <Post/>
     </div>
+    </Provider>
   );
 }
 
